5301373e82086b5b199e7fced08bdb48f5875391
Lisa Walters <lisacampiwalters@gmail.com>
2023-02-15 00:51:42 -0700

    declared and initialized variables, and then created a sentence using interpolation.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //declaring and initializing variables
+            string greeting = "Hello! It is very nice to meet you!";
+            int age = 5;
+            char grade = 'A';
+            bool isSleepy = true;
+            double amountDue = 5.5;
+            decimal height = 5.2m;
+
+
+            //interpolation
+
+            Console.WriteLine($"Sometimes, the child would say to strangers, '{greeting}', which was a problem because she was only {age} years old.Fortunately, she tended to earn the grade of {grade} in school, even though it is {isSleepy} that she was sleepy quite often.Nonetheless, she paid the {amountDue} that she owed, and then was fined even more money because whe was only {height} feet tall.");
+
+
         }
     }
 }
